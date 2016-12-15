c220a1a8e8f680133030c9a1e490824b52ed6481
Jan Weinschenker <jan.weinschenker@holisticon.de>
2016-12-15 13:52:10 +0100

    SSLContextCreator entfernt und App aufgeräumt

--- src/main/java/de/holisticon/jdk9/App.java
+++ src/main/java/de/holisticon/jdk9/App.java
@@ -1,6 +1,5 @@
 package de.holisticon.jdk9;
 
-import de.holisticon.jdk9.http2.util.SSLContextCreator;
 import de.holisticon.jdk9.http2.util.StreamListener;
 import org.apache.log4j.Logger;
 import org.eclipse.jetty.http.HttpFields;
@@ -11,16 +10,12 @@ import org.eclipse.jetty.http2.api.Session;
 import org.eclipse.jetty.http2.api.Stream;
 import org.eclipse.jetty.http2.api.server.ServerSessionListener;
 import org.eclipse.jetty.http2.client.HTTP2Client;
-import org.eclipse.jetty.http2.frames.DataFrame;
 import org.eclipse.jetty.http2.frames.HeadersFrame;
-import org.eclipse.jetty.util.Callback;
 import org.eclipse.jetty.util.FuturePromise;
-import org.eclipse.jetty.http2.frames.PushPromiseFrame;
 import org.eclipse.jetty.util.Jetty;
 import org.eclipse.jetty.util.Promise;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
 
-import javax.net.ssl.SSLContext;
 import java.net.InetSocketAddress;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Phaser;
@@ -30,9 +25,9 @@ import java.util.concurrent.TimeoutException;
 /**
  * Hello world!
  *
- * @see https://webtide.com/the-new-jetty-9-http-client/
- * @see http://git.eclipse.org/c/jetty/org.eclipse.jetty.project.git/tree/jetty-http2/http2-client/src/test/java/org/eclipse/jetty/http2/client/Client.java
- * @see https://blogs.oracle.com/brewing-tests/entry/http_2_with_jetty_server
+ * @see <a href="https://webtide.com/the-new-jetty-9-http-client/">the-new-jetty-9-http-client</a>
+ * @see <a href="http://git.eclipse.org/c/jetty/org.eclipse.jetty.project.git/tree/jetty-http2/http2-client/src/test/java/org/eclipse/jetty/http2/client/Client.java">Jetty HTTP2Client Example</a>
+ * @see <a href="https://blogs.oracle.com/brewing-tests/entry/http_2_with_jetty_server">http_2_with_jetty_server</a>
  */
 public class App {
 
@@ -40,28 +35,23 @@ public class App {
 
     public static void main(String[] args) {
         App app = new App();
-        app.startClient();
-    }
-
-    public void startClient() {
         String host = "nghttp2.org";
         int port = 443;
-        HTTP2Client httpClient = createHttpClient(host, port);
+        String path = "/#/";
+        app.performHttpRequest(host, port, path);
     }
 
-    private HTTP2Client createHttpClient(String host, int port) {
+    public HTTP2Client performHttpRequest(String host, int port, String path) {
         HTTP2Client http2Client = new HTTP2Client();
         FuturePromise<Session> sessionPromise = new FuturePromise<>();
         SslContextFactory contextFactory = new SslContextFactory(true);
-        contextFactory.setTrustAll(true);
         http2Client.addBean(contextFactory);
 
         try {
             http2Client.start();
             http2Client.connect(contextFactory, new InetSocketAddress(host, port), new ServerSessionListener.Adapter(), sessionPromise);
-            //Session session = sessionPromise.get(5, TimeUnit.SECONDS);
-            Session session = sessionPromise.get();
-            HeadersFrame requestHeaders = getRequestHeaders(host, port, http2Client);
+            Session session = sessionPromise.get(5, TimeUnit.SECONDS);
+            HeadersFrame requestHeaders = getRequestHeaders(host, port, path, http2Client);
             final Phaser phaser = new Phaser(2);
             session.newStream(requestHeaders, new Promise.Adapter<Stream>(), new StreamListener(phaser));
 
@@ -80,19 +70,21 @@ public class App {
         return http2Client;
     }
 
-    private HeadersFrame getRequestHeaders(String host, int port, HTTP2Client http2Client) {
+    /**
+     * Create the necessary request headers.
+     *
+     * @param host
+     * @param port
+     * @param path
+     * @param http2Client
+     * @return
+     */
+    private HeadersFrame getRequestHeaders(String host, int port, String path, HTTP2Client http2Client) {
         HttpFields requestFields = new HttpFields();
         requestFields.put("User-Agent", http2Client.getClass().getName() + "/" + Jetty.VERSION);
-        MetaData.Request metaData = new MetaData.Request("GET", new HttpURI("https://" + host + ":" + port + "/#/"), HttpVersion.HTTP_2, requestFields);
+        MetaData.Request metaData = new MetaData.Request("GET", new HttpURI("https://" + host + ":" + port + path), HttpVersion.HTTP_2, requestFields);
         HeadersFrame headersFrame = new HeadersFrame(metaData, null, true);
         return headersFrame;
-    }
-
-    private SslContextFactory getSslContextFactory() {
-        SSLContext sslContext = SSLContextCreator.getContextInstance();
-        SslContextFactory contextFactory = new SslContextFactory(true);
-        contextFactory.setSslContext(sslContext);
-        return contextFactory;
     }
 
 }
